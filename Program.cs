f8824b975a114aed0fa8e51dad4eb73d9546da40
Piotr Zarzycki <piotr.zarzyckidev@proton.me>
2025-09-29 14:03:17 +0200

    Zadanie wykonane

--- Program.cs
+++ Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] names = new string[] { "Ania", "Kasia", "Basia", "Zosia" };
+        string[] surnames = new string[] { "Kowalska", "Nowak" };
+
+        using StreamWriter writer = new("dane.csv", false);
+        writer.WriteLine("lp,name,surname,birthYear");
+        Random random = new();
+        for (int i = 1; i < 101; i++)
+        {
+            var name = names[random.Next(0, names.Length)];
+            var surname = surnames[random.Next(0, surnames.Length)];
+            writer.WriteLine($"{i},{name},{surname},{random.Next(1989,2001)}");
+        }
     }
 }
